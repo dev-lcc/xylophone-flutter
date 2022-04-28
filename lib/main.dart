24b3b40f0cbbdbae96f4121e8517f3e250eb6b27
Lawrence C <52389524+rsvp-lawrencec@users.noreply.github.com>
2022-04-28 10:34:49 +0800

    Xylophone App Challenge Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,65 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final audioPlayer = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Container(),
+        appBar: AppBar(
+          title: Text("Shiela Marie's Xylophone 😘"),
         ),
-      ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: colorSet.map((e) {
+                int index = colorSet.indexOf(e);
+                return xylophoneKey(index, () {
+                  audioPlayer.play("note${index + 1}.wav");
+                });
+              }).toList(),
+            ),
+          ),
+        ),
+      )
     );
   }
+}
+
+const colorSet = [
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.pink,
+  Colors.indigo,
+  Colors.yellow,
+  Colors.teal,
+];
+
+Widget xylophoneKey(int index, Function onPressed) {
+  return Expanded(
+    child: Row(
+      // mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: TextButton(
+            child: Text(
+                "Key #${index + 1}",
+              style: TextStyle(color: Colors.black),
+            ),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(colorSet[index]),
+            ),
+            onPressed: onPressed,
+          ),
+        )
+      ],
+    ),
+  );
 }
